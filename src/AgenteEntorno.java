193833877f73d56b6d2bcc15e0eea8ea1ad6b586
antonio <antonio@mosquito>
2014-11-14 13:47:34 +0100

    Comentando traza del programa

--- src/AgenteEntorno.java
+++ src/AgenteEntorno.java
@@ -69,7 +69,7 @@ public class AgenteEntorno extends SingleAgent {
 
         while (vivo) {
             ACLMessage inbox = null, outbox = null;
-
+            System.out.println("Agente Entorno: Esperando recibir mensajes del servidor.");
             // Recibir todos los mensajes de los sensores
             for (int i = 0; i < 4 && vivo; i++) {
                 String strJson = null;
@@ -92,14 +92,20 @@ public class AgenteEntorno extends SingleAgent {
                 else if (parser.contains(strJson, "scanner")) {
                     result = parser.getElement(json, "scanner");
                     this.scanner = parser.jsonElementToArrayFloat(result);
+                    
+                    System.out.println("Agente Entorno: Recibido scanner: "+this.scanner);
                 } // Recibir los datos del radar
                 else if (parser.contains(strJson, "radar")) {
                     result = parser.getElement(json, "radar");
                     this.radar = parser.jsonElementToArrayInt(result);
+                    
+                    System.out.println("Agente Entorno: Recibido radar: "+this.radar);
                 } // Recibir el nivel de la batería
                 else if (parser.contains(strJson, "bateria")) {
                     result = parser.getElement(json, "bateria");
                     this.nivelBateria = result.getAsFloat();
+                    
+                    System.out.println("Agente Entorno: Recibido bateria: "+this.nivelBateria);
                 } // Recibir la posición del bot
                 else if (parser.contains(strJson, "gps")) {
                     result = parser.getElement(json, "gps");
@@ -107,7 +113,10 @@ public class AgenteEntorno extends SingleAgent {
                     this.coord.setX(resultDentro.getAsInt());
                     resultDentro = parser.getElement(result, "y");
                     this.coord.setY(resultDentro.getAsInt());
+                    
+                    System.out.println("Agente Entorno: Recibido gps: "+this.coord.getX() + " " + this.coord.getY());
                 }
+                
             }
 
             if (vivo) {
@@ -184,11 +193,12 @@ public class AgenteEntorno extends SingleAgent {
                 outbox = new ACLMessage();
                 outbox.setSender(this.getAid());
                 outbox.setReceiver(this.agenteBot);
-
+                System.out.println("Agente Entorno: Enviando mensaje a botprincipal");
                 LinkedHashMap lhm = new LinkedHashMap();
                 lhm.put("bateria", this.nivelBateria);
-
+                
                 outbox.setContent(parser.crearJson(lhm));
+                System.out.println("Agente Entorno: Enviado nivel de batería : "+this.nivelBateria);
             }
         }
     }
