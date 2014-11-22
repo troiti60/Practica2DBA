414e984fb3357d9533a81c4d4ed4c4eb7d9d795a
antonio <antonio@mosquito>
2014-11-22 12:16:22 +0100

    Añadida función de pruebas test3 para el método de mapa: getLastPerception

--- test/TestMapa.java
+++ test/TestMapa.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**Clase de testeo para la clase mapa (y nodo)
@@ -11,7 +12,7 @@ import java.util.HashMap;
  * @author antonio
  */
 public class TestMapa {
-    public static int testea = 2;
+    public static int testea = 3;
     /**
     * Testeo básico del HashMap y las funciones equals y hashCode de Coord
     * así como de Integer
@@ -55,32 +56,32 @@ public class TestMapa {
     */
     public static void test2(){
         Mapa map= Mapa.crearInstancia();
-        map.addNodo(new Nodo(2, 2, 0, 10));
+        map.addNodo(new Nodo(2, 2, 0, 0));
         
-        map.addNodo(new Nodo(0, 0, 0, 0));
-        map.addNodo(new Nodo(1, 0, 0, 1));
-        map.addNodo(new Nodo(2, 0, 0, 0));
-        map.addNodo(new Nodo(3, 0, 1, 1));
-        map.addNodo(new Nodo(4, 0, 0, 0));
-        map.addNodo(new Nodo(0, 1, 0, 1));
-        map.addNodo(new Nodo(1, 1, 0, 0));
-        map.addNodo(new Nodo(2, 1, 0, 1));
-        map.addNodo(new Nodo(3, 1, 1, 0));
-        map.addNodo(new Nodo(4, 1, 0, 1));
-        map.addNodo(new Nodo(0, 2, 0, 1));
-        map.addNodo(new Nodo(1, 2, 0, 0));
-        map.addNodo(new Nodo(3, 2, 1, 0));
-        map.addNodo(new Nodo(4, 2, 0, 1));
-        map.addNodo(new Nodo(0, 3, 0, 1));
-        map.addNodo(new Nodo(1, 3, 0, 0));
-        map.addNodo(new Nodo(2, 3, 0, 1));
-        map.addNodo(new Nodo(3, 3, 1, 0));
-        map.addNodo(new Nodo(4, 3, 0, 1));
-        map.addNodo(new Nodo(0, 4, 0, 1));
-        map.addNodo(new Nodo(1, 4, 0, 0));
-        map.addNodo(new Nodo(2, 4, 0, 1));
-        map.addNodo(new Nodo(3, 4, 1, 0));
-        map.addNodo(new Nodo(4, 4, 0, 1));
+        map.addNodo(new Nodo(0, 0, 0, 1));
+        map.addNodo(new Nodo(1, 0, 0, 2));
+        map.addNodo(new Nodo(2, 0, 0, 3));
+        map.addNodo(new Nodo(3, 0, 1, 4));
+        map.addNodo(new Nodo(4, 0, 0, 5));
+        map.addNodo(new Nodo(0, 1, 0, 6));
+        map.addNodo(new Nodo(1, 1, 0, 7));
+        map.addNodo(new Nodo(2, 1, 0, 8));
+        map.addNodo(new Nodo(3, 1, 1, 9));
+        map.addNodo(new Nodo(4, 1, 0, 10));
+        map.addNodo(new Nodo(0, 2, 0, 11));
+        map.addNodo(new Nodo(1, 2, 0, 12));
+        map.addNodo(new Nodo(3, 2, 1, 14));
+        map.addNodo(new Nodo(4, 2, 0, 15));
+        map.addNodo(new Nodo(0, 3, 0, 16));
+        map.addNodo(new Nodo(1, 3, 0, 17));
+        map.addNodo(new Nodo(2, 3, 0, 18));
+        map.addNodo(new Nodo(3, 3, 1, 19));
+        map.addNodo(new Nodo(4, 3, 0, 20));
+        map.addNodo(new Nodo(0, 4, 0, 21));
+        map.addNodo(new Nodo(1, 4, 0, 22));
+        map.addNodo(new Nodo(2, 4, 0, 23));
+        map.addNodo(new Nodo(3, 4, 1, 24));
+        map.addNodo(new Nodo(4, 4, 0, 25));
         
         //inicializamos el mapa de la siguiente manera:
         
@@ -136,11 +137,11 @@ public class TestMapa {
         
         De esta forma, todos los nodos del mapa deverían estar en conectado.
         */
-        map.addNodo(new Nodo(0, 5, 0, 10));
-        map.addNodo(new Nodo(1, 5, 0, 10));
-        map.addNodo(new Nodo(2, 5, 0, 10));
-        map.addNodo(new Nodo(3, 5, 0, 10));
-        map.addNodo(new Nodo(4, 5, 0, 10));
+        map.addNodo(new Nodo(0, 5, 0, 26));
+        map.addNodo(new Nodo(1, 5, 0, 27));
+        map.addNodo(new Nodo(2, 5, 0, 28));
+        map.addNodo(new Nodo(3, 5, 0, 29));
+        map.addNodo(new Nodo(4, 5, 0, 30));
         conectados=map.getConectado();
         noconectados=map.getNoConectado();
         System.out.println("Comprobamos que los nodos están añadidos donde deben2\n\n");
@@ -175,7 +176,55 @@ public class TestMapa {
         
         }
         System.out.println("\n\n");
-
+        map.setCoord(new Coord(2,3));
+    }
+    /**
+     * Método test para probar el método de mapa getLastPerception.
+     * Se introduce elsiguiente mapa:
+     * X X X X X
+     * X X X X X
+     * X X B 0 0
+     * X X 0 0 0
+     * X X 0 0 0
+     */
+    public static void test3(){
+        Mapa map = Mapa.crearInstancia();
+        map.addNodo(new Nodo(0,0,0,13));
+        map.addNodo(new Nodo(-2, -2, 1, 1));
+        map.addNodo(new Nodo(-1, -2, 1, 2));
+        map.addNodo(new Nodo(0, -2, 1, 3));
+        map.addNodo(new Nodo(1,-2, 1, 4));
+        map.addNodo(new Nodo(2, -2, 1, 5));
+        map.addNodo(new Nodo(-2, -1, 1, 6));
+        map.addNodo(new Nodo(-1, -1, 1, 7));
+        map.addNodo(new Nodo(0, -1, 1, 8));
+        map.addNodo(new Nodo(1, -1, 1, 9));
+        map.addNodo(new Nodo(2, -1, 1, 10));
+        map.addNodo(new Nodo(-2, 0, 1, 11));
+        map.addNodo(new Nodo(-1, 0, 1, 12));
+        map.addNodo(new Nodo(1, 0, 0, 14));
+        map.addNodo(new Nodo(2, 0, 0, 15));
+        map.addNodo(new Nodo(-2, 1, 1, 16));
+        map.addNodo(new Nodo(-1, 1, 1, 17));
+        map.addNodo(new Nodo(0, 1, 0, 18));
+        map.addNodo(new Nodo(1, 1, 0, 19));
+        map.addNodo(new Nodo(2, 1, 0, 20));
+        map.addNodo(new Nodo(-2, 2, 1, 21));
+        map.addNodo(new Nodo(-1, 2, 1, 22));
+        map.addNodo(new Nodo(0, 2, 0, 23));
+        map.addNodo(new Nodo(1, 2, 0, 24));
+        map.addNodo(new Nodo(2, 2, 0, 25));
+        map.setCoord(new Coord(0,0));
+        ArrayList<Nodo> perception = map.getLastPerception();
+        String cad= new String();
+        for(int i=0;i<25;i++){
+            cad+= perception.get(i).getScanner()+" ";
+            if(i%5==4){
+                System.out.println(cad);
+                cad=new String();
+            }
+        }
+    
     }
     
     public static void main(String[] args) { 
@@ -185,6 +234,9 @@ public class TestMapa {
             test1();
         else if (testea==2)
             test2();
+        else if (testea==3)
+            test3();
+ 
 
     }
     
